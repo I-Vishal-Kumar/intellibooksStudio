5775396cf3e3ee45e8d7915c47b7c2ba9bbcce4d
Abhishek Kumar <abhishek.kumar@jaiinfoway.com>
2025-12-19 16:48:33 +0530

    add signup|signin routes using clerk

--- apps/ui/app/sign-up/[[...sign-up]]/page.tsx
+++ apps/ui/app/sign-up/[[...sign-up]]/page.tsx
@@ -23,6 +23,7 @@ export default function SignUpPage() {
           routing="path"
           path="/sign-up"
           signInUrl="/sign-in"
+          afterSignUpUrl="/"
           fallbackRedirectUrl="/"
         />
       </div>
